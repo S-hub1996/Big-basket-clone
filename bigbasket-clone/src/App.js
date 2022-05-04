eeb845d542b5e7de0102f4f985a27351a6b5d287
Mohammad Saif <saifsami321md@gmail.com>
2022-05-04 19:57:13 +0530

    first commit

--- bigbasket-clone/src/App.js
+++ bigbasket-clone/src/App.js
@@ -1,11 +1,11 @@
-// import Header from "./Components/Header & Footer/Header/Header";
+import Header from "./Components/Header & Footer/Header/Header";
 // import { Footer } from "./Components/Header & Footer/Footer/Footer";
 import product from "./Components/Product_page/product";
 function App() {
   return (
     <div className="App">
       {/* <h1>hello</h1> */}
-      {/* <Header/> */}
+      <Header/>
       {/* <Footer/> */}
       <product/>
     </div>
