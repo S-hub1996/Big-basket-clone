3f9113302c6a69fe1310bbcaab14959c652516e5
Mohd Haroon Qureshi <qureshiharoon010203@gmail.com>
2022-05-04 10:49:07 +0530

    store setup and react-router-dom

--- bigbasket-clone/src/App.js
+++ bigbasket-clone/src/App.js
@@ -1,9 +1,10 @@
 
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>hello</h1>
 
     </div>
   );
---
+++ bigbasket-clone/src/Redux/store.js
@@ -0,0 +1,41 @@
+import {
+  combineReducers,
+  compose,
+  applyMiddleware,
+  legacy_createStore as createStore,
+} from "redux";
+
+// import reducers
+
+
+
+// add your reducers here
+const rootReducer = combineReducers({
+// add here
+});
+
+// this is middle-ware
+const middleware1 = (store) => (next) => (action) => {
+  console.log("type of action is ", action);
+  //if action is function invoke this
+  if (typeof action === "function") {
+    return action(store.dispatch);
+  }
+  next(action); // mandatory thing
+};
+
+/// foe redux tool
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(middleware1)
+  // other store enhancers if any
+);
+
+// this is store
+export const store = createStore(rootReducer, enhancer);
