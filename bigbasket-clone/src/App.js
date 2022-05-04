6257e66e2a9f077f3cbe9c3d1aab466537192f6f
Shubham <sv7836832607@gmail.com>
2022-05-04 19:39:25 +0530

    Revert "first commit"
    This reverts commit 4337aab65abcc816a1119f4a6e668361b71b54a5.

--- bigbasket-clone/src/App.js
+++ bigbasket-clone/src/App.js
@@ -1,13 +1,11 @@
-// import Header from "./Components/Header & Footer/Header/Header";
-// import { Footer } from "./Components/Header & Footer/Footer/Footer";
-import product from "./Components/Product_page/product";
+import Header from "./Components/Header & Footer/Header/Header";
+import { Footer } from "./Components/Header & Footer/Footer/Footer";
 function App() {
   return (
     <div className="App">
       {/* <h1>hello</h1> */}
-      {/* <Header/> */}
-      {/* <Footer/> */}
-      <product/>
+      <Header/>
+      <Footer/>
     </div>
   );
 }
--- bigbasket-clone/src/Components/Product_page/product.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const product = () => {
-  return (
-    <div><h1>Product page</h1></div>
-  )
-}
-
-export default product
