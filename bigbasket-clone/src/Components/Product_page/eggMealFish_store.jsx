63e1feb95bfc80b5c90b97dc7315210f05779bc6
Codex-Swapnil1 <swapnilparchakewrd2@gmail.com>
2022-05-07 23:51:47 +0530

    update data
    add id and quantity

--- bigbasket-clone/src/Components/Product_page/eggMealFish_store.jsx
+++ bigbasket-clone/src/Components/Product_page/eggMealFish_store.jsx
@@ -1,136 +1,157 @@
 var eggMealFish = [
-    {
-      OFF: 24,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName: "Eggs - Regular",
-      productprice: 92.92,
-      StrikePrice: 130,
-      Brand: "Fresho",
-      Date: "Standard Delivery : 10AM To 6PM",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/1203477_2-fresho-eggs-regular.jpg",
-    },
-    {
-      OFF: 34,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName: "Chicken Breast - Boneless, Antibiotic Residue-Free",
-      productprice: 430,
-      StrikePrice: 690,
-      Brand: "Fresho",
-      Date: "Standard Delivery : 10AM To 6PM",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/10001001_7-fresho-chicken-breast-boneless-antibiotic-residue-free.jpg",
-    },
-    {
-      OFF: 24,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName: "Farm Eggs - Table Tray, Medium, Antibiotic Residue-Free",
-      productprice: 216,
-      StrikePrice: 275,
-      Brand: "Fresho",
-      Date: "Standard Delivery : 10AM To 6PM",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/150502_6-fresho-farm-eggs-table-tray-medium-antibiotic-residue-free.jpg",
-    },
-    {
-      OFF: 45,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName: "Chicken Breast - Boneless",
-      productprice: 137.75,
-      StrikePrice: 145,
-      Brand: "Zorabian",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/30001413_5-zorabian-chicken-breast-boneless.jpg",
-    },
-    {
-      OFF: 42,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName:
-        "Fresho Chicken Curry Cut Without Skin 1 kg + Kara Coconut Milk - Classic 400 ml",
-      productprice: 384.65,
-      StrikePrice: 595,
-      Brand: "BB-Combo",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/1213732_1-bb-combo-fresho-chicken-curry-cut-without-skin-1-kg-kara-coconut-milk-classic-400-ml.jpg",
-    },
-    {
-      OFF: 45,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName: "Chicken Curry Cut 1kg + Seer Fish Slice/Steaks 500 g",
-      productprice: 991,
-      StrikePrice: 1752,
-      Brand: "Fresho",
-      Date: "Standard Delivery : 10AM To 6PM",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/1215095_1-fresho-chicken-curry-cut-1kg-seer-fish-slicesteaks-500-g.jpg",
-    },
-    {
-      OFF: 21,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName:
-        "Fresho Meat Chicken Curry Cut Without Skin 1kg + Everest Chicken Masala 100g",
-      productprice: 331.2,
-      StrikePrice: 539,
-      Brand: "BB-Combo",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/1202514_1-bb-combo-fresho-meat-chicken-curry-cut-without-skin-1kg-everest-chicken-masala-100g.jpg",
-    },
-    {
-      OFF: 22,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName:
-        "Fresho Chicken Breast Boneless 4-5 pcs 500g +Kara Coconut Milk UHT Classic 200ml",
-      productprice: 332.15,
-      StrikePrice: 425,
-      Brand: "BB-Combo",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/1213733_1-bb-combo-fresho-chicken-breast-boneless-4-5-pcs-500g-kara-coconut-milk-uht-classic-200ml.jpg",
-    },
-    {
-      OFF: 24,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName:
-        "Rohu Fish - Large, Curry Cut/Bengali Cut, Preservative Free, 12-15 pcs",
-      productprice: 413.36,
-      StrikePrice: 620,
-      Brand: "Fresho",
-      Date: "Standard Delivery : 10AM To 6PM",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/10000756_34-fresho-rohu-fish-large-curry-cutbengali-cut-preservative-free-12-15-pcs.jpg",
-    },
-    {
-      OFF: 54,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName: "Desi Rohu Fish Steak - Pre Marinated, Ready To Cook",
-      productprice: 269,
-      StrikePrice: 763,
-      Brand: "Fresho",
-      Date: "Standard Delivery : 10AM To 6PM",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/s/40240474_4-fresho-desi-rohu-fish-steak-pre-marinated-ready-to-cook.jpg",
-    },
-    {
-      OFF: 55,
-      QtyPiece: "Price of 1 Kg",
-      QtyPieceForOneElement: "1 Kg",
-      productName:
-        "Chicken Leg - Without Skin, Antibiotic Residue-Free, 3-4 pcs",
-      productprice: 325,
-      StrikePrice: 435,
-      Brand: "Fresho",
-      Date: "Standard Delivery : 10AM To 6PM",
-      image:
-        "https://www.bigbasket.com/media/uploads/p/mm/10000912_9-fresho-chicken-leg-without-skin-antibiotic-residue-free-3-4-pcs.jpg",
-    },
-  ];
-  export default eggMealFish;
+  {
+    id: 510,
+    quantity:0,
+    OFF: 24,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName: "Eggs - Regular",
+    productprice: 92.92,
+    StrikePrice: 130,
+    Brand: "Fresho",
+    Date: "Standard Delivery : 10AM To 6PM",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/1203477_2-fresho-eggs-regular.jpg",
+  },
+  {
+    id: 511,
+    quantity:0,
+    OFF: 34,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName: "Chicken Breast - Boneless, Antibiotic Residue-Free",
+    productprice: 430,
+    StrikePrice: 690,
+    Brand: "Fresho",
+    Date: "Standard Delivery : 10AM To 6PM",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/10001001_7-fresho-chicken-breast-boneless-antibiotic-residue-free.jpg",
+  },
+  {
+    id: 512,
+    quantity:0,
+    OFF: 24,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName: "Farm Eggs - Table Tray, Medium, Antibiotic Residue-Free",
+    productprice: 216,
+    StrikePrice: 275,
+    Brand: "Fresho",
+    Date: "Standard Delivery : 10AM To 6PM",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/150502_6-fresho-farm-eggs-table-tray-medium-antibiotic-residue-free.jpg",
+  },
+  {
+    id: 513,
+    quantity:0,
+    OFF: 45,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName: "Chicken Breast - Boneless",
+    productprice: 137.75,
+    StrikePrice: 145,
+    Brand: "Zorabian",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/30001413_5-zorabian-chicken-breast-boneless.jpg",
+  },
+  {
+    id: 514,
+    quantity:0,
+    OFF: 42,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName:
+      "Fresho Chicken Curry Cut Without Skin 1 kg + Kara Coconut Milk - Classic 400 ml",
+    productprice: 384.65,
+    StrikePrice: 595,
+    Brand: "BB-Combo",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/1213732_1-bb-combo-fresho-chicken-curry-cut-without-skin-1-kg-kara-coconut-milk-classic-400-ml.jpg",
+  },
+  {
+    id: 515,
+    quantity:0,
+    OFF: 45,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName: "Chicken Curry Cut 1kg + Seer Fish Slice/Steaks 500 g",
+    productprice: 991,
+    StrikePrice: 1752,
+    Brand: "Fresho",
+    Date: "Standard Delivery : 10AM To 6PM",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/1215095_1-fresho-chicken-curry-cut-1kg-seer-fish-slicesteaks-500-g.jpg",
+  },
+  {
+    id: 516,
+    quantity:0,
+    OFF: 21,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName:
+      "Fresho Meat Chicken Curry Cut Without Skin 1kg + Everest Chicken Masala 100g",
+    productprice: 331.2,
+    StrikePrice: 539,
+    Brand: "BB-Combo",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/1202514_1-bb-combo-fresho-meat-chicken-curry-cut-without-skin-1kg-everest-chicken-masala-100g.jpg",
+  },
+  {
+    id: 517,
+    quantity:0,
+    OFF: 22,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName:
+      "Fresho Chicken Breast Boneless 4-5 pcs 500g +Kara Coconut Milk UHT Classic 200ml",
+    productprice: 332.15,
+    StrikePrice: 425,
+    Brand: "BB-Combo",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/1213733_1-bb-combo-fresho-chicken-breast-boneless-4-5-pcs-500g-kara-coconut-milk-uht-classic-200ml.jpg",
+  },
+  {
+    id: 518,
+    quantity:0,
+    OFF: 24,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName:
+      "Rohu Fish - Large, Curry Cut/Bengali Cut, Preservative Free, 12-15 pcs",
+    productprice: 413.36,
+    StrikePrice: 620,
+    Brand: "Fresho",
+    Date: "Standard Delivery : 10AM To 6PM",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/10000756_34-fresho-rohu-fish-large-curry-cutbengali-cut-preservative-free-12-15-pcs.jpg",
+  },
+  {
+    id: 519,
+    quantity:0,
+    OFF: 54,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName: "Desi Rohu Fish Steak - Pre Marinated, Ready To Cook",
+    productprice: 269,
+    StrikePrice: 763,
+    Brand: "Fresho",
+    Date: "Standard Delivery : 10AM To 6PM",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/s/40240474_4-fresho-desi-rohu-fish-steak-pre-marinated-ready-to-cook.jpg",
+  },
+  {
+    id: 520,
+    quantity:0,
+    OFF: 55,
+    QtyPiece: "Price of 1 Kg",
+    QtyPieceForOneElement: "1 Kg",
+    productName:
+      "Chicken Leg - Without Skin, Antibiotic Residue-Free, 3-4 pcs",
+    productprice: 325,
+    StrikePrice: 435,
+    Brand: "Fresho",
+    Date: "Standard Delivery : 10AM To 6PM",
+    image:
+      "https://www.bigbasket.com/media/uploads/p/mm/10000912_9-fresho-chicken-leg-without-skin-antibiotic-residue-free-3-4-pcs.jpg",
+  },
+];
